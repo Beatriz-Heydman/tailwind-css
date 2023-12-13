dc82902c83d9447912265b541210d89b1fe2d76c
Beatriz Heydman <108696912+Beatriz-Heydman@users.noreply.github.com>
2023-12-12 22:08:56 -0300

    feat: create theme tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,22 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        "gray-900": "#141414",
+        "blue-100": "#7DD3FC",
+        "blue-200": "#1B284E",
+        "blue-300": "#2D3344",
+        "blue-400": "#1b2335",
+        "blue-500": "#0F172A",
+      },
       fontFamily: {
         sans: ["Poppins", "sans-serif"],
       },
+      fontWeight: {
+        'light': 300,
+        'normal': 400,
+        'medium': 500,     
+      }
     },
   },
   plugins: [],
