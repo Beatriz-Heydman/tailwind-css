6e2b8e8c08a97cce63b6b7e9d675665f19c01b20
Beatriz Heydman <108696912+Beatriz-Heydman@users.noreply.github.com>
2023-12-18 19:26:32 -0300

    feat: create nav-item

---
+++ src/components/header/nav-item/index.tsx
@@ -0,0 +1,15 @@
+export function NavItem() {
+  return (
+    <nav className=" flex gap-4 text-white text-sm">
+      <a href="" className="transition-all duration-700 hover:text-blue-100">
+        Página Inicial
+      </a>
+      <a href="" className="transition-all duration-700 hover:text-blue-100">
+        Classes
+      </a>
+      <a href="" className="transition-all duration-700 hover:text-blue-100">
+        Docs
+      </a>
+    </nav>
+  );
+}
